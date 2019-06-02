88800aa646655542ba8daf81d2e90e23d189ac6a
Jakub Giminski <jakubgiminski@gmail.com>
2019-06-02 21:35:14 +0100

    Remove old files

--- src/Serialization/Serializable.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace RestApiCreator\Serialization;
-
-interface Serializable
-{
-    public function serialize(Serializer $serializer);
-}
--- src/Serialization/Serializer.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace RestApiCreator\Serialization;
-
-interface Serializer
-{
-    public function serialize(array $data);
-}
